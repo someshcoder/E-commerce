f250864d8e33150e4ccfe5c8e94b02881e4de44a
Somesh Bhatnagar <someshbhatnagar544@gmail.com>
2026-01-09 22:36:13 +0530

    update some files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { CartProvider } from "./context/CartContext";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
+import TopDealDetail from "./pages/TopDealDetail";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -10,10 +11,11 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Profile from "./pages/Profile";
 import Settings from './pages/Settings';
+import AdminDashboard from './pages/AdminDashboard';
+import Checkout from './pages/Checkout';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Wishlist from './components/Wishlist';
-
 
 function App() {
   return (
@@ -27,12 +29,20 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/products" element={<Products />} />
           <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/topdeal/:id" element={<TopDealDetail />} />
           <Route path="/wishlist" element={<Wishlist />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/settings" element={<Settings />} />
+          
+          {/* Admin Routes */}
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/products" element={<AdminDashboard />} />
+          <Route path="/admin/users" element={<AdminDashboard />} />
+          <Route path="/admin/orders" element={<AdminDashboard />} />
         </Routes>
 
         <Footer />
