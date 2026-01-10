d4aa673a50149aa80705274bd7fb5ccfd73656d3
Somesh Bhatnagar <someshbhatnagar544@gmail.com>
2026-01-10 16:25:53 +0530

    need some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import Checkout from './pages/Checkout';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Wishlist from './components/Wishlist';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -24,25 +25,28 @@ function App() {
         <Header />
 
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-          <Route path="/topdeal/:id" element={<TopDealDetail />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/settings" element={<Settings />} />
+          {/* Public Routes - Accessible to everyone */}
+          <Route path="/" element={<ProtectedRoute isPublic={true}><Home /></ProtectedRoute>} />
+          <Route path="/about" element={<ProtectedRoute isPublic={true}><About /></ProtectedRoute>} />
+          <Route path="/contact" element={<ProtectedRoute isPublic={true}><Contact /></ProtectedRoute>} />
+          <Route path="/login" element={<ProtectedRoute isPublic={true}><Login /></ProtectedRoute>} />
+          <Route path="/register" element={<ProtectedRoute isPublic={true}><Register /></ProtectedRoute>} />
+          
+          {/* Private Routes - Require authentication */}
+          <Route path="/products" element={<ProtectedRoute><Products /></ProtectedRoute>} />
+          <Route path="/product/:id" element={<ProtectedRoute><ProductDetails /></ProtectedRoute>} />
+          <Route path="/topdeal/:id" element={<ProtectedRoute><TopDealDetail /></ProtectedRoute>} />
+          <Route path="/wishlist" element={<ProtectedRoute><Wishlist /></ProtectedRoute>} />
+          <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+          <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
           
           {/* Admin Routes */}
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-          <Route path="/admin/products" element={<AdminDashboard />} />
-          <Route path="/admin/users" element={<AdminDashboard />} />
-          <Route path="/admin/orders" element={<AdminDashboard />} />
+          <Route path="/admin/dashboard" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+          <Route path="/admin/products" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+          <Route path="/admin/users" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+          <Route path="/admin/orders" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
         </Routes>
 
         <Footer />
