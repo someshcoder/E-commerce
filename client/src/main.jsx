755b61283a50a51c149fdc5f81f46adfbc15d94e
Somesh Bhatnagar <someshbhatnagar544@gmail.com>
2026-01-10 13:17:51 +0530

    remove Wishlist Provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'   
 import { CartProvider } from "./context/CartContext"
-import { WishlistProvider } from './context/WishlistContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WishlistProvider>
     <CartProvider>
       <App />
     </CartProvider>
-    </WishlistProvider>
   </React.StrictMode>,
 )
