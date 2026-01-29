e270fdededd109e21a22c793cd98891b53392bfb
Somesh Bhatnagar <someshbhatnagar544@gmail.com>
2026-01-29 23:04:43 +0530

    update Server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,8 @@ connectDB();
 
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // ✅ ROUTES
 app.use("/api/auth", require("./routes/authRoutes"));
