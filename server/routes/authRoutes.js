9ea6b7a5fd23c76e6bb3dd1ed9ebe350dac51728
Somesh Bhatnagar <someshbhatnagar544@gmail.com>
2026-01-30 23:30:36 +0530

    Update authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const {
   registerUser,
   loginUser,
+  updateProfile,
 } = require("../controllers/authController");
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.put("/profile", protect, updateProfile);
 
 module.exports = router;
